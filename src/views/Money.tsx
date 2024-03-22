20e8c375f06ac62fe424c6731dd71dfd367883bf
midbezoar <2974490550@qq.com>
2024-03-22 15:48:38 +0800

    feat: money.tsx UI 完成

--- src/views/Money.tsx
+++ src/views/Money.tsx
@@ -5,6 +5,11 @@ import React from "react";
 const TagsSection = styled.section`
   background: #fff;
   padding: 12px 16px;
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: flex-start;
   > ol {
     margin: 0 -12px;
     > li {
@@ -71,11 +76,70 @@ const CategorySection = styled.section`
   }
 `;
 
-const NumberPadSection = styled.section``;
+const NumberPadSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  > .output {
+    background-color: #fff;
+    font-size: 36px;
+    line-height: 72px;
+    text-align: right;
+    padding: 0 16px;
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25), inset 0 5px 5px -5px rgb(0, 0, 0, 0.25);
+  }
+  > .pad {
+    /* display: flex;
+    flex-wrap: wrap; */
+    > button {
+      font-size: 18px;
+      float: left;
+      width: 20%;
+      height: 64px;
+      border: none;
+      &.ok {
+        height: 128px;
+        float: right;
+      }
+      &:nth-child(1) {
+        background-color: #f2f2f2;
+      }
+      &:nth-child(2),
+      &:nth-child(5) {
+        background-color: #e0e0e0;
+      }
+      &:nth-child(3),
+      &:nth-child(6),
+      &:nth-child(9) {
+        background-color: #d3d3d3;
+      }
+      &:nth-child(4),
+      &:nth-child(7),
+      &:nth-child(10) {
+        background-color: #c1c1c1;
+      }
+      &:nth-child(8),
+      &:nth-child(11),
+      &:nth-child(13) {
+        background-color: #b8b8b8;
+      }
+      &:nth-child(12) {
+        background-color: #9a9a9a;
+      }
+      &:nth-child(14) {
+        background-color: #a9a9a9;
+      }
+    }
+  }
+`;
+
+const MyLayout = styled(Layout)`
+  display: flex;
+  flex-direction: column;
+`;
 
 function Money() {
   return (
-    <Layout>
+    <MyLayout>
       <TagsSection>
         <ol>
           <li>衣</li>
@@ -98,25 +162,30 @@ function Money() {
         </ul>
       </CategorySection>
       <NumberPadSection>
-        <div>100</div>
-        <div>
+        <div className='output'>100</div>
+        <div className='pad clearfix'>
           <button>1</button>
           <button>2</button>
           <button>3</button>
+          <button>+</button>
           <button>删除</button>
           <button>4</button>
           <button>5</button>
           <button>6</button>
+          <button>-</button>
           <button>清空</button>
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
-          <button>.</button>
+          <button>X</button>
+          <button className='ok'>OK</button>
+          <button className='zero'>0</button>
+          <button className='dot'>.</button>
+          <button>%</button>
+          <button>/</button>
         </div>
       </NumberPadSection>
-    </Layout>
+    </MyLayout>
   );
 }
 
