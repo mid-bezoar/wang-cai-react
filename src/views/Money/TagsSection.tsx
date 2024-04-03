f4de62d6e4c4ed0ef18b1539deca65b563170ac9
midbezoar <2974490550@qq.com>
2024-04-03 13:57:51 +0800

    完成标签的选择和新增

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
+import React, { useState } from "react";
 import styled from "styled-components";
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #fff;
   padding: 12px 16px;
   flex-grow: 1;
@@ -17,6 +18,9 @@ const TagsSection = styled.section`
       padding: 4px 18px;
       font-size: 14px;
       margin: 8px 12px;
+      &.selected {
+        background: #f60f60;
+      }
     }
   }
   > button {
@@ -28,5 +32,41 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `;
+
+const TagsSection: React.FC = (props) => {
+  const [tags, setTags] = useState<string[]>([]);
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+
+  const onAddTag = () => {
+    const tagName = window.prompt("新标签的名称为：");
+    if (tagName) {
+      setTags([...tags, tagName]);
+    }
+  };
+
+  const onToggleTag = (tag: string) => {
+    const index = selectedTags.indexOf(tag);
+    if (index >= 0) {
+      setSelectedTags(selectedTags.filter((t) => t !== tag));
+    } else {
+      setSelectedTags([...selectedTags, tag]);
+    }
+  };
+
+  const getTagClass = (tag: string) => (selectedTags.indexOf(tag) >= 0 ? "selected" : "");
+
+  return (
+    <Wrapper>
+      <ol>
+        {tags.map((tag) => (
+          <li key={tag} onClick={() => onToggleTag(tag)} className={getTagClass(tag)}>
+            {tag}
+          </li>
+        ))}
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  );
+};
 
 export { TagsSection };
